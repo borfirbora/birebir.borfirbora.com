044e46c620ba20470401cf97443c3a9655a21753
babaprogramlar <programlarbaba@gmail.com>
2024-07-07 21:32:42 +0300

    'updated'

--- src/components/CalEmbed.jsx
+++ src/components/CalEmbed.jsx
@@ -1,18 +1,19 @@
 import Cal, { getCalApi } from "@calcom/embed-react";
-  import { useEffect } from "react";
-  export default function CalEmbed({CalURL}) {
-	useEffect(()=>{
-	  (async function () {
-		const cal = await getCalApi({});
-		cal("ui", {"styles":{"branding":{"brandColor":"#000000"}},"hideEventTypeDetails":true,"layout":"month_view"});
-	  })();
+import { useEffect, useRef } from "react";
+export default function CalEmbed({ CalURL }) {
+
+
+	useEffect(() => {
+		(async function () {
+			const cal = await getCalApi({});
+			cal("ui", { "styles": { "branding": { "brandColor": "#000000" } }, "hideEventTypeDetails": true, "layout": "month_view" });
+		})();
 	}, [])
-	return <Cal 
-	  calLink={CalURL}
-	  style={{width:"100%",height:"100%",overflow:"scroll"}}
-	  config={{layout: 'month_view'}}
-    
-	  
+	return <Cal
+		calLink={CalURL}
+		style={{ width: "100%", height: "100%", overflow: "scroll" }}
+		config={{ layout: 'month_view' }}
+
+
 	/>;
-  };
-  
+};
